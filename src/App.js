c19dd6bb5078fe3dc14a98c7155422f2b3f5ed17
Shayanne11 <shayanneirlanda@gmail.com>
2021-05-29 02:12:01 -0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+
+const UserLogin = () => {
+  const fetchUserLogin = () => {
+    axios.get("http://localhost:8080/client/").then(res =>{
+      console.log(res);
+    });
+  }
+  useEffect ( () =>{
+fetchUserLogin();
+  }, [] );
+return <h1> oieee</h1>
+
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Tela de Login</h1>
-      </header>
+    <UserLogin></UserLogin>
+
+
+      {/* <header className="App-header">
+        <h1>Tela de Login da shay deu certo? ebaaa</h1>
+      </header> */}
     </div>
   );
 }
