82f160f518420e515347432b0e23ded1ed823e0d
Shayanne11 <shayanneirlanda@gmail.com>
2021-05-29 05:35:06 -0300

    add pages file

---
+++ src/pages/Login/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './login.css'
+
+const Login = () => {
+  return (
+    <div className="login">
+      <h1> aeeeeeee</h1>
+      <head>
+<meta charset="UTF-8"></meta>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </head>
+
+      <body onload="iniciar()">
+        <div class="container">
+          <div class="content">
+            <div  id="card-login" class="card-login">
+              <div class="form-login" >
+                <div class="titulo-card">ENTRAR NA MINHA CONTA</div>
+
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+        
+         </body>
+
+
+
+    </div>
+  )
+}
