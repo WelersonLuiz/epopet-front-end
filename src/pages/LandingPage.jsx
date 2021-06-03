f11eee878a96a591d52e2a444374aeb816aa1421
Jones-Coelho <jonesmcoelho@gmail.com>
2021-06-03 00:04:15 -0300

    testes landingpage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -13,11 +13,16 @@ class LandingPage extends Component{
         super(props);
     }
 
+    GoToLogin = (e) => {
+        this.props.history.push("/login")
+    }
+
     render(){
         return(
             <div>
                 <div className='no_login_cabecalho'>
-                    <h1>CABECALHO</h1>
+
+                    <Button className='no_login_cabecalho_botao_login' onClick={this.GoToLogin}>Login</Button>
 
                 </div>
                 <div className='no_login_rodape'>
@@ -25,7 +30,17 @@ class LandingPage extends Component{
 
                 </div>
                 <div className='no_login_esquerda'>
-                    <h1>ESQUERDA</h1>
+                    <p>Lorem Ipsum is simply dummy 
+                       text of the printing and typesetting industry. 
+                       Lorem Ipsum has been the industry's standard dummy 
+                       text ever since the 1500s, when an unknown printer 
+                       took a galley of type and scrambled it to make a type
+                       specimen book. It has survived not only five centuries, 
+                       but also the leap into electronic typesetting, remaining 
+                       essentially unchanged. It was popularised in the 1960s with the 
+                       release of Letraset sheets containing Lorem Ipsum passages, 
+                       and more recently with desktop publishing software like Aldus 
+                       PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 <div className='no_login_direita'>
                     <h1>DIREITA</h1>
