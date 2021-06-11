bd2818c82816363f118f4ec6bea98fd9ea53c0b6
Jones-Coelho <jonesmcoelho@gmail.com>
2021-06-11 00:32:22 -0300

    Landing Page, about e planos, quase ok

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import axios from "axios";
-
-import { Link } from "react-router-dom";
 
 import "./LandingPage.css"
 
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import {Header} from './common'
+
+import logobig from '../images/banner.png'
+import nomelogobig from '../images/logobigSize.png'
+import petbanner from '../images/petbanner.jpg'
+
+import { CardGroup, Card, Button} from 'react-bootstrap'
 
 class LandingPage extends Component{
     constructor(props){
@@ -17,12 +18,21 @@ class LandingPage extends Component{
     render(){
         return(
             <div>
-            <div>
                 <Header/>
-            </div> 
-            <div className='container'>
-
-            </div>
+                <div className='logo_main'>
+                    <img src={logobig} className="img_logo"/>
+                    <img src={nomelogobig} className="img_nome_logo"/>
+                </div>
+                <CardGroup style={{maxHeight:"25%",maxWidth:"55%",position:"relative",marginTop:"29%",marginLeft:"25%"}}>
+                    <Card style={{alignItems:"center",border:"none"}}>
+                        <Card.Text style={{textAlign:"right"}}>There variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</Card.Text>
+                        <Button href='/nos' style={{maxHeight:"20%",maxWidth:"50%"}}>Saiba mais</Button>
+                    </Card>
+                    <Card style={{alignItems:"center",border:"none"}}>
+                        <Card.Img variant="bottom" src={petbanner} style={{height:"75%",width:"75%"}}></Card.Img>
+                    </Card>
+                </CardGroup>
+                
             </div>
 
         );
