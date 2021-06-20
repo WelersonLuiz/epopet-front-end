5f7ee2e38ee8b5019d26a21fd5ec3a9b0ee562fd
Jones-Coelho <jonesmcoelho@gmail.com>
2021-06-20 08:46:19 -0300

    commit before merge

--- src/pages/dashboardPage/DashboardPage.jsx
+++ src/pages/dashboardPage/DashboardPage.jsx
@@ -23,6 +23,7 @@ class DashboardPage extends Component {
         <h1>You're logged in!</h1>
         <h1>Status: {window.$isLoggedIn}</h1>
         <button onClick={() => this.handleLogout()}>Logout</button>
+        <button onClick={() => this.props.history.push('/pets')}>Pets</button>
       </div>
     );
   }
